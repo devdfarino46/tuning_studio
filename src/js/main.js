e524f74963a11b6a85454d47941c89066cb50301
devdfarino46 <dev.dfarino46@gmail.com>
2023-04-28 23:54:47 +0300

    Added animation and state

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@ const menuElem = document.querySelector('.nav__links-wrapper');
 
 closeMenuBtn.addEventListener('click', function() {
     menuElem.classList.remove('nav__links-wrapper--menu');
-
+    document.body.classList.remove('noscrolling');
 });
 
 openMenuBtn.addEventListener('click', function() {
     menuElem.classList.add('nav__links-wrapper--menu');
-    
+    document.body.classList.add('noscrolling');
 });
